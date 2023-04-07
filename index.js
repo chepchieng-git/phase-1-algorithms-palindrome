54dc66e5ce4646754826e9295a888d620024cc58
Wycliffe <wycliffekiprono4@gmail.com>
2023-04-07 16:59:36 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const lowerCasedString = word.toLowerCase()
+  const splitString = lowerCasedString.split("")
+  const splitReverseString = splitString.reverse()
+  const joinString = splitReverseString.join("");
+  if(lowerCasedString !== joinString)
+  {
+    return false
+  }else
+    return true
+  }
+
 
 /* 
   Add your pseudocode here
+  Write the  string input
+  Ensure you convert it to lowerCase
+  reverse the string 
+  Then compare the string both reversed
+  return true when string is equal 
+  return false if string is not equal
 */
 
 /*
   Add written explanation of your solution here
+  I made Conversion of input to lowercase
+I Splitted string to be in an array
+Ithen reversed the splitted string
+Joined the string and compared the original with the reversed string
 */
 
 // You can run `node index.js` to view these console logs
